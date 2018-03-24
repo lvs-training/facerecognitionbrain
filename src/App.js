5ed3adf1414caf4e9fe7cbe94ab3b4063374c838
laurent <laurent@langkawi.localdomain>
2018-03-24 10:33:33 +0100

    simple server connection test

--- src/App.js
+++ src/App.js
@@ -39,6 +39,12 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+      .then(console.log)
+  }
+
   calculateFaceLocation = (data) => {
       const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
       const image = document.getElementById('imputimage');
@@ -55,6 +61,8 @@ class App extends Component {
   displayFaceBox = (box) => {
     this.setState({box: box})
   }
+
+
 
   onInputChange = (event) => {
     this.setState({input: event.target.value});
